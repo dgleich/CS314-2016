80f89ec52c3fc4719e9f7c8eb68127b05dd6cfc2
dgleich <dgleich@gmail.com>
2016-09-08 19:43:34 +0000

    juliabox 2016-09-08 19:43:34.236430+00:00

--- get_links.jl
+++ get_links.jl
@@ -1,12 +1,12 @@
 import Requests
 function get_links_big(node)
-    r = Requests.get(@sprintf("http://puma.cs.purdue.edu:8888/node/%i", node))
+    r = Requests.get("http://puma.cs.purdue.edu:8888/node/"*string(node))
     @assert(r.status == 200)
     str= ASCIIString(r.data)
     return map( x -> parse(Int64,x), split(str," "))
 end
 function get_links_small(node)
-    r = Requests.get(@sprintf("http://puma.cs.purdue.edu:9999/node/%i", node))
+    r = Requests.get("http://puma.cs.purdue.edu:9999/node/"*string(node))
     @assert(r.status == 200)
     str= ASCIIString(r.data)
     return map( x -> parse(Int64,x), split(str," "))
